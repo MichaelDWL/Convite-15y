f915a2fb70e6574ed7d95e85466a7cf809890226
MichaelDWL <michaeldouglas.devs@gmail.com>
2025-02-16 00:31:35 -0300

    update-responsive

---
+++ wpp.js
@@ -0,0 +1,18 @@
+function enviarWpp() {
+    var name = document.getElementById("name").value;
+    var number = document.getElementById("number").value;
+    // var texto = "Nome: ${nome}\nSetor:${setor}\nRamal:${ramal}\nLocal:${local}\nSolicitacao:${solicitacao}\nUrgecia:${urgencia}";
+    // var numeroWhatsapp = "5531992214044";
+   
+    var url = "https://wa.me/5531994370758?text=" //ADICIONE SEU N°
+    
+    // estrutura do texto
+
++ "*Confirmação de Presença*" + "%0a" 
++ "%0a"
++ "*Nome*: " + name + "%0a" + "%0a"
++ "*Numero*: " + number + "%0a" + "%0a"
+
+window.open(url, '_blank').focus();
+
+}
